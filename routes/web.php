f1dfa0bac8aac0c4709876a5edbed546b8c8947e
ryoei-o <ryoei3408@outlook.jp>
2024-01-25 20:22:36 +0000

    8-5終了

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
-
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
